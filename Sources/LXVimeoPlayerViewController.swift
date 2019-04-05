1f6744811b8aa0eef3a06d5a4fa293d9b03e8184
satish <satish.v@canopas.com>
2019-04-05 13:03:47 +0530

    set delegate

--- Sources/LXVimeoPlayerViewController.swift
+++ Sources/LXVimeoPlayerViewController.swift
@@ -15,8 +15,15 @@ open class LXVimeoPlayerController: UIViewController {
     }
     override open func loadView() {
         self.view = LXVimeoPlayerView()
+        self.playerView.delegate = self
     }
     override open func viewDidLoad() {
         super.viewDidLoad()
+    }
+}
+
+extension LXVimeoPlayerController: LXVimeoPlayerViewDelegate {
+    public func vimeoPlayer(_ vimeoPlayer: LXVimeoPlayerView, on event: LXVimeoPlayerView.PlayerEvent) {
+        
     }
 }
